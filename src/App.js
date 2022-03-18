6361b2411412186157d17f083576e459709e2cd4
Sebastian <szuviria@gmail.com>
2022-03-17 21:59:59 -0300

    Codigo de after 17/03/2022

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 // import logo from './logo.svg';
 // import './App.css';
-import Avatar from './components/Avatar/Avatar'
+// import Avatar from './components/Avatar/Avatar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import NavBar from './components/NavBar/NavBar';
-import ClassCount from "./components/ClassCount/ClassCount";
-import FunctionCounter from "./components/FunctionCounter/FunctionCounter";
-
-
-
+// import ClassCount from "./components/ClassCount/ClassCount";
+// import FunctionCounter from "./components/FunctionCounter/FunctionCounter";
 
 function App() {
-
   return (
     <div className="App">
       <NavBar />
       <ItemListContainer greeting="Hola Coders"/>
-      <Avatar name="Sebastian" age="28" country="Argentina">
+      {/* <Avatar name="Sebastian" age="28" country="Argentina">
         <button>Boton</button>
       </Avatar>
       <Avatar name="Renzo" age="25" country="Uruguay"> 
         Texto de prueba
       </Avatar>
       <FunctionCounter />
-      <ClassCount />
+      <ClassCount /> */}
     </div>
   );
 }
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,6 +1,20 @@
-const ItemListContainer = ({ greeting }) => {
+import React from 'react'
+
+const names = ['Seba', 'Tomas', 'Veronica', 'Luiggi', 'Gustavo']
+const otherNames = [{ id: 1, name: 'Seba', color: 'red'}, { id: 2, name: 'Tomas', color: 'blue' }, { id: 3, name: 'Veronica', color: 'green'}]
+
+const ItemListContainer = (props) => {
+
+
     return(
-        <h1>{greeting}</h1>
+        <>
+            <h1>{props.greeting}</h1>
+            <ul>
+               { names.map(name => React.createElement('li', { key: name, style: { color: 'red' } }, name))} 
+               { names.map(name => <li key={name} style={{ color: 'red'}}>{name}</li>)}
+               { otherNames.map(objName => <li key={objName.id} style={{ color: objName.color}}>{objName.name}</li>)}
+            </ul>
+        </>
     )
 }
 
